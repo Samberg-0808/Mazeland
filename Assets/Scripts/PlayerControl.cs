bb11f9f0e8e292539ea29be42bb1295597abce23
yangyintang <yintangy@usc.edu>
2022-10-01 12:15:28 -0700

    Add Level Info & Fix bugs

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControl : MonoBehaviour
 {
 
     public float movementSpeed = 3.0f;
+    public TextMeshPro PlayerText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerText.text = 0.ToString();
     }
 
     // Update is called once per frame

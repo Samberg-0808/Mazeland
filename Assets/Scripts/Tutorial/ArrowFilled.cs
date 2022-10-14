c5f564252c913e7818402bf451cc80a1557d2e22
xiujing20 <xiujingh@usc.edu>
2022-10-14 10:45:28 -0700

    add tutorial

---
+++ Assets/Scripts/Tutorial/ArrowFilled.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowFilled: MonoBehaviour
+{
+    public Image image;
+    public float progressPercentage;
+    public float target = 100;
+
+    void Start()
+    {
+        image.fillAmount = 1f;
+    }
+}

6ad7f317fc8cb029b25ccb6dc47c341eb5575196
Chuanshi Zhu <chuanshizhu@seismic.com>
2022-10-15 14:44:34 -0700

    add lv8 and lv9
    add lv8 and lv9

---
+++ Assets/Scripts/Level8/spawnerGenerator_lv8.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spawnerGenerator_lv8 : MonoBehaviour
+{
+    private int i = 0;
+
+    public GameObject[] coins;
+
+    public GameObject[] enemies;
+
+    public GameObject[] items;
+
+    public GameObject[] immune_items;
+
+    public int CoinsControl;
+
+    public int EnemiesControl;
+
+    public int startGameTiming;
+
+    // **** data code ****
+    public int totalEnemy;
+    public int totalCoins;
+    public int totalItems;
+    // ********
+
+    public int coinsLimit;
+    public int enemiesLimit;
+
+    public int currentCoins;
+    public int currentEnemies;
+    public Scene scene;
+
+
+
+    List<Vector3> CoinVectors = new List<Vector3>();
+
+    List<Vector3> EnemyVectors = new List<Vector3>();
+
+    List<Vector3> itemVectors = new List<Vector3>();
+
+    List<Vector3> immuneItemVectors = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+
+        // **** data code ****
+        totalEnemy = 0;
+        totalCoins = 0;
+        totalItems = 0;
+        // ********
+        
+        currentCoins = 0;
+        currentEnemies = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        if (i > startGameTiming)
+        {
+            if (i % CoinsControl == 0 & currentCoins < coinsLimit)
+            {
+                spawnCoins();
+                currentCoins++;
+
+                // **** data code ****
+                totalCoins++;
+                // ********
+            }
+
+            if (i % EnemiesControl == 0 & currentEnemies < enemiesLimit)
+            {
+                // **** data code ****
+                totalEnemy++;
+                // ********
+                currentEnemies++;
+                spawnEnemies();
+            }
+
+            if (i % 1000 == 0 & scene.name == "Level8")
+            {
+                spawnItems();
+
+                // **** data code ****
+                totalItems++;
+                // ********
+            }
+        }
+    }
+
+
+    public void spawnCoins()
+    {
+        int r = Random.Range(0, coins.Length);
+
+        Vector2 center = new Vector2(1.07f, 0.58f);
+
+        Vector2 randomPoint = center + Random.insideUnitCircle * 4f;
+
+        Instantiate(coins[r], randomPoint, transform.rotation);
+
+
+
+    }
+
+
+
+    public void spawnEnemies()
+    {
+        int r = Random.Range(0, enemies.Length);
+
+        Vector2 center = new Vector2(1.07f, 0.58f);
+
+        Vector2 randomPoint = center + Random.insideUnitCircle * 4f;
+
+        Instantiate(enemies[r], randomPoint, transform.rotation);
+    }
+
+  
+
+    void spawnItems()
+    {
+        int r = Random.Range(0, items.Length);
+
+        Vector2 center = new Vector2(1.07f, 0.58f);
+
+        Vector2 randomPoint = center + Random.insideUnitCircle * 4f;
+
+        Instantiate(items[r], randomPoint, transform.rotation);
+
+    }
+
+    public void spawnImmuneItems() {
+        int r = Random.Range(0, items.Length);
+
+        Vector2 center = new Vector2(1.07f, 0.58f);
+
+        Vector2 randomPoint = center + Random.insideUnitCircle * 4f;
+
+        Instantiate(immune_items[r], randomPoint, transform.rotation);
+    }
+}

2d227bf664566bb73db8580fb10ca67b3f29791f
xiujing20 <xiujingh@usc.edu>
2022-10-17 00:13:26 -0700

    update tutorial

---
+++ Assets/Scripts/Tutorial/TutorialRestartMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialRestartMenu : MonoBehaviour
+{
+    public Button ButtonYes;
+    public Button ButtonNo;
+    public TextMeshProUGUI skipText;
+}

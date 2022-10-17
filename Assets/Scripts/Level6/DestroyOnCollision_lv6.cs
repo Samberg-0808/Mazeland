752fd17bffbfa7603dfa287333b40a137543352f
xiujing20 <xiujingh@usc.edu>
2022-10-16 21:26:21 -0700

    update tutorial, level menu and level selection

--- Assets/Scripts/Level6/DestroyOnCollision_lv6.cs
+++ Assets/Scripts/Level6/DestroyOnCollision_lv6.cs
@@ -115,7 +115,7 @@ public class DestroyOnCollision_lv6 : MonoBehaviour
             levelTime = levelTime / 1000;
             long currLevel = 6;
             sc.Send(_sessionID, currLevel, levelTime, -1, life.life);
-            sc.enemySend(sg.totalEnemy, enemyKilled, sg.totalCoins, pointGained, sg.totalItems, itemGained);
+            //sc.enemySend(sg.totalEnemy, enemyKilled, sg.totalCoins, pointGained, sg.totalItems, itemGained);
             // ********
 
             //OnPlayerScore?.Invoke();
---
+++ Assets/Scripts/Level6/Level6_UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level6_UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gameOverMenu; 
+
+    private void OnEnable() {
+        DestroyOnCollision_lv6.OnPlayerScore += EnableGameOverMenu;
+    }
+
+    private void OnDisable() {
+        DestroyOnCollision_lv6.OnPlayerScore -= EnableGameOverMenu;
+    }
+
+    public void EnableGameOverMenu() {
+        gameOverMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void ReloadMainScene(){
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+}
+

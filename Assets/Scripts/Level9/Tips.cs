e2bf95a96ec0b074e9e0d33104a4a8663b5544e6
xiujing20 <xiujingh@usc.edu>
2022-11-01 17:20:10 -0700

    fixed ui and tip time

--- Assets/Scripts/Level9/Tips.cs
+++ Assets/Scripts/Level9/Tips.cs
@@ -8,7 +8,7 @@ public class Tips : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 5.0f);
+        Destroy(gameObject, 2.0f);
     }
 
     // Update is called once per frame
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 }
